6a1ab2f327e396dc89a854f357bad3cc6d9d11b0
adars <adars@LAPTOP-DSNAJO0O>
2021-09-02 13:16:15 +0530

    new Test case TC04 is added

---
+++ maven/src/test/java/kite/Test_04Test.java
@@ -0,0 +1,14 @@
+package kite;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test_04Test {
+	
+	@Test
+	
+	public static void chats() {
+		Reporter.log("invoice should be added",true);
+	}
+
+}
