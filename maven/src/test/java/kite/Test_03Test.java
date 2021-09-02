7c7d38750f9393e45120a10939f7a169fdbfeaf4
adars <adars@LAPTOP-DSNAJO0O>
2021-09-02 13:13:03 +0530

    added TC03Test

--- maven/src/test/java/kite/Test_03Test.java
+++ maven/src/test/java/kite/Test_03Test.java
@@ -3,12 +3,12 @@ package kite;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class Test_02Test {
+public class Test_03Test {
 	
 	@Test
 	
 	public static void chats() {
-		Reporter.log("account should be deleted",true);
+		Reporter.log("account should be added",true);
 	}
 
 }
